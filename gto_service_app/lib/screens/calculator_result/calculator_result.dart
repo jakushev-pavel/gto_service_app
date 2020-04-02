ede0f22d10520f5249f64de30e752f2b8829740a
jakushev-pavel <no.2889@yandex.ru>
2020-04-02 18:40:21 +0300

    Перенес все виджеты экрана результатов калькулятора в один файл

--- gto_service_app/lib/screens/calculator_result/calculator_result.dart
+++ gto_service_app/lib/screens/calculator_result/calculator_result.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:gtoserviceapp/models/calculator.dart';
-import 'package:gtoserviceapp/screens/calculator_result/result.dart';
+import 'package:gtoserviceapp/models/trials.dart';
+import 'package:gtoserviceapp/services/api/api.dart';
 import 'package:provider/provider.dart';
 
 class CalculatorResultScreen extends StatelessWidget {
@@ -10,7 +11,79 @@ class CalculatorResultScreen extends StatelessWidget {
     var calc = Provider.of<CalculatorModel>(context, listen: false);
     return Scaffold(
       appBar: AppBar(title: Text("Калькулятор")),
-      body: Result(calc.age, calc.gender),
+      body: _buildBody(calc.age, calc.gender),
+    );
+  }
+
+  Widget _buildBody(int age, Gender gender) {
+    return FutureBuilder(
+      future: API.I.fetchTrials(age, gender),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return _buildTrials(snapshot.data, age, gender);
+        }
+        if (snapshot.hasError) {
+          return Text(snapshot.error.toString());
+        }
+        return Center(child: CircularProgressIndicator());
+      },
+    );
+  }
+
+
+  Widget _buildTrials(TrialsModel trials, int age, Gender gender) {
+    return Column(
+      children: <Widget>[
+        Text("Пол: ${gender.toStr()}"),
+        Text("Возраст: $age"),
+        Text(trials.ageCategory),
+        Expanded(child: _buildList(trials)),
+      ],
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+    );
+  }
+
+  ListView _buildList(trials) {
+    return ListView.builder(
+      itemBuilder: (context, index) {
+        if (index >= trials.length) {
+          return null;
+        }
+        return ListTile(
+          title: _buildTrial(trials.at(index)),
+        );
+      },
+    );
+  }
+
+  Widget _buildTrial(TrialModel trial) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(trial.trialName),
+        Row(
+          children: <Widget>[
+            _buildTrialResult(trial.resultForGold, Colors.yellow),
+            _buildTrialResult(trial.resultForSilver, Colors.grey),
+            _buildTrialResult(trial.resultForBronze, Colors.brown.shade300),
+          ],
+        )
+      ],
+    );
+  }
+
+  Widget _buildTrialResult(String value, Color color) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 4.0),
+      child: Row(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 4.0),
+            child: Icon(Icons.brightness_1, color: color),
+          ),
+          Text(value),
+        ],
+      ),
     );
   }
 }
