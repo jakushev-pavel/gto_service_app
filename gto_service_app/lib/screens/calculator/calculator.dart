bc0b4a6c84ea1e2c9a454bb691095960f1b3e4a0
jakushev-pavel <no.2889@yandex.ru>
2020-04-04 04:54:39 +0300

    Улучшена верстка калькулятора

--- gto_service_app/lib/screens/calculator/calculator.dart
+++ gto_service_app/lib/screens/calculator/calculator.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:gtoserviceapp/components/navigation/nav_bar.dart';
 import 'package:gtoserviceapp/components/navigation/tabs.dart';
