7153264e02b86f8165d85a86b9d40b32ad0a827c
jakushev-pavel <no.2889@yandex.ru>
2020-04-04 14:09:54 +0300

    Вынесен заголовок профиля в отдельный класс

---
+++ gto_service_app/lib/screens/profile/header.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:gtoserviceapp/components/dialogs/error_dialog.dart';
+import 'package:gtoserviceapp/components/layout/expanded_horizontally.dart';
+import 'package:gtoserviceapp/components/layout/shrunk_vertically.dart';
+import 'package:gtoserviceapp/services/api/api.dart';
+import 'package:gtoserviceapp/services/api/models.dart';
+import 'package:gtoserviceapp/services/storage/keys.dart';
+import 'package:gtoserviceapp/services/storage/storage.dart';
+
+class ProfileHeader extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ShrunkVertically(
+      child: Card(
+        margin: EdgeInsets.all(16),
+        child: Padding(
+          padding: EdgeInsets.all(16),
+          child: ExpandedHorizontally(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                _buildUserName(context),
+                _buildRole(context),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  FutureBuilder<GetUserInfoResponse> _buildUserName(context) {
+    var future = API.I.getUserInfo();
+    ErrorDialog.showOnFutureError(context, future);
+
+    return FutureBuilder(
+      future: future,
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return Text(
+            snapshot.data.name,
+            style: Theme.of(context).textTheme.headline,
+          );
+        }
+
+        return CircularProgressIndicator();
+      },
+    );
+  }
+
+  Widget _buildRole(context) {
+    return Text(
+      Storage.I.read(Keys.role),
+      style: Theme.of(context).textTheme.caption,
+    );
+  }
+}
--- gto_service_app/lib/screens/profile/profile.dart
+++ gto_service_app/lib/screens/profile/profile.dart
@@ -1,15 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:gtoserviceapp/components/dialogs/error_dialog.dart';
-import 'package:gtoserviceapp/components/layout/expanded_horizontally.dart';
-import 'package:gtoserviceapp/components/layout/shrunk_vertically.dart';
 import 'package:gtoserviceapp/components/navigation/nav_bar.dart';
 import 'package:gtoserviceapp/components/navigation/tabs.dart';
 import 'package:gtoserviceapp/screens/login/login.dart';
-import 'package:gtoserviceapp/services/api/api.dart';
-import 'package:gtoserviceapp/services/api/models.dart';
 import 'package:gtoserviceapp/services/auth/auth.dart';
-import 'package:gtoserviceapp/services/storage/keys.dart';
-import 'package:gtoserviceapp/services/storage/storage.dart';
+
+import 'header.dart';
 
 class ProfileScreen extends StatelessWidget {
   @override
@@ -27,55 +22,8 @@ class ProfileScreen extends StatelessWidget {
         automaticallyImplyLeading: false,
         actions: <Widget>[_buildLogoutButton(context)],
       ),
-      body: _buildBody(context),
+      body: ProfileHeader(),
       bottomNavigationBar: NavigationBar(Tabs.Profile),
-    );
-  }
-
-  Widget _buildBody(context) {
-    return ShrunkVertically(
-      child: Card(
-        margin: EdgeInsets.all(16),
-        child: Padding(
-          padding: EdgeInsets.all(16),
-          child: ExpandedHorizontally(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                _buildUserName(context),
-                _buildRole(context),
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-
-  FutureBuilder<GetUserInfoResponse> _buildUserName(context) {
-    var future = API.I.getUserInfo();
-    ErrorDialog.showOnFutureError(context, future);
-
-    return FutureBuilder(
-      future: future,
-      builder: (context, snapshot) {
-        if (snapshot.hasData) {
-          return Text(
-            snapshot.data.name,
-            style: Theme.of(context).textTheme.headline,
-          );
-        }
-
-        return CircularProgressIndicator();
-      },
-    );
-  }
-
-  Widget _buildRole(context) {
-    return Text(
-      Storage.I.read(Keys.role),
-      style: Theme.of(context).textTheme.caption,
     );
   }
 
