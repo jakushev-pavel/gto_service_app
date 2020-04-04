b9f0bf1aad86fcfbab6da0cffaeea6149b1f872c
jakushev-pavel <no.2889@yandex.ru>
2020-04-04 14:18:34 +0300

    Вынесен AppBar профиля в отдельный класс

--- gto_service_app/lib/screens/profile/profile.dart
+++ gto_service_app/lib/screens/profile/profile.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:gtoserviceapp/components/navigation/nav_bar.dart';
 import 'package:gtoserviceapp/components/navigation/tabs.dart';
 import 'package:gtoserviceapp/screens/login/login.dart';
+import 'package:gtoserviceapp/screens/profile/app_bar.dart';
 import 'package:gtoserviceapp/services/auth/auth.dart';
 
 import 'header.dart';
@@ -17,27 +18,9 @@ class ProfileScreen extends StatelessWidget {
 
   Widget _build(context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Профиль"),
-        automaticallyImplyLeading: false,
-        actions: <Widget>[_buildLogoutButton(context)],
-      ),
+      appBar: ProfileAppBar(),
       body: ProfileHeader(),
       bottomNavigationBar: NavigationBar(Tabs.Profile),
     );
-  }
-
-  Widget _buildLogoutButton(context) {
-    return IconButton(
-      icon: Icon(Icons.arrow_forward),
-      onPressed: () => _onLogoutButtonPressed(context),
-    );
-  }
-
-  _onLogoutButtonPressed(context) {
-    Auth.I.logout();
-    Navigator.of(context).pushReplacement(MaterialPageRoute(
-      builder: (_) => LoginScreen(),
-    ));
   }
 }
