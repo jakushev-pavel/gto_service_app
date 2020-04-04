c3a030a9f9164acc92a726b11c46a4aa990dcf3e
jakushev-pavel <no.2889@yandex.ru>
2020-04-04 06:50:19 +0300

    ErrorDialog наследуется от OkDialog

--- gto_service_app/lib/components/dialogs/error_dialog.dart
+++ gto_service_app/lib/components/dialogs/error_dialog.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:gtoserviceapp/components/dialogs/ok_dialog.dart';
 import 'package:gtoserviceapp/services/api/api_error.dart';
 
-class ErrorDialog extends StatelessWidget {
-  final String _text;
+class ErrorDialog extends OkDialog {
+  static const _title = "Ошибка";
 
-  ErrorDialog.fromText(this._text);
+  ErrorDialog.fromText(text) : super(_title, text: text);
 
   static _errorToText(Exception e) {
     if (e is APIErrors) {
@@ -14,7 +15,7 @@ class ErrorDialog extends StatelessWidget {
     return e.toString();
   }
 
-  ErrorDialog.fromError(e) : _text = _errorToText(e);
+  ErrorDialog.fromError(e) : super(_title, text: _errorToText(e));
 
   static showOnFutureError<T>(BuildContext context, Future<T> future) {
     future.catchError(
@@ -24,20 +25,6 @@ class ErrorDialog extends StatelessWidget {
           child: ErrorDialog.fromError(error),
         )
       },
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return AlertDialog(
-      title: Text("Ошибка"),
-      content: _text == null ? null : Text(_text),
-      actions: <Widget>[
-        FlatButton(
-          child: Text("Ok"),
-          onPressed: () => Navigator.of(context).pop(),
-        )
-      ],
     );
   }
 }
